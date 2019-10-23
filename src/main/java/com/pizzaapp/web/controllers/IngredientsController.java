d404d4452f858a429d2aeb7056754768c8956576
StefanUrilski <urilskistefan@gmail.com>
2019-10-23 11:34:01 +0300

    Initial commit. Major ingredient form and controller fixed.

--- src/main/java/com/pizzaapp/web/controllers/IngredientsController.java
+++ src/main/java/com/pizzaapp/web/controllers/IngredientsController.java
@@ -1,17 +1,10 @@
 package com.pizzaapp.web.controllers;
 
-import com.pizzaapp.domain.models.binding.ingredients.*;
-import com.pizzaapp.domain.models.service.ingredients.*;
 import com.pizzaapp.service.IngredientService;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 @RequestMapping("menu/ingredients/add")
