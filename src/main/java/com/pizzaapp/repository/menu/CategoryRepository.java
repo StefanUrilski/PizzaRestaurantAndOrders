8aa284eca302a6cb08b16a5363835035b27929b7
StefanUrilski <urilskistefan@gmail.com>
2019-10-23 12:52:53 +0300

    CategoryServiceImpl class logic and CategoryRepository method added created.

--- src/main/java/com/pizzaapp/repository/menu/CategoryRepository.java
+++ src/main/java/com/pizzaapp/repository/menu/CategoryRepository.java
@@ -5,5 +5,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface CategoryRepository extends JpaRepository<Category, String> {
 
-
+    Category findByName(String name);
 }
--- src/main/java/com/pizzaapp/service/CategoryServiceImpl.java
+++ src/main/java/com/pizzaapp/service/CategoryServiceImpl.java
@@ -1,12 +1,14 @@
 package com.pizzaapp.service;
 
 import com.pizzaapp.domain.models.service.ingredients.CategoryServiceModel;
+import com.pizzaapp.domain.models.service.ingredients.CheeseServiceModel;
 import com.pizzaapp.repository.menu.CategoryRepository;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 public class CategoryServiceImpl implements CategoryService {
@@ -22,11 +24,14 @@ public class CategoryServiceImpl implements CategoryService {
 
     @Override
     public CategoryServiceModel getCategoryByName(String name) {
-        return null;
+        return modelMapper.map(categoryRepository.findByName(name), CategoryServiceModel.class);
     }
 
     @Override
     public List<CategoryServiceModel> getAllCategories() {
-        return null;
+        return categoryRepository.findAll()
+                .stream()
+                .map(category -> modelMapper.map(category, CategoryServiceModel.class))
+                .collect(Collectors.toList());
     }
 }
