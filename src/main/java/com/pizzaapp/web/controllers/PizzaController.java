553d1ac2ce304df430e9901da2dd494a19a2ffea
StefanUrilski <urilskistefan@gmail.com>
2019-10-27 14:25:59 +0200

    PizzaController class created.

---
+++ src/main/java/com/pizzaapp/web/controllers/PizzaController.java
@@ -0,0 +1,13 @@
+package com.pizzaapp.web.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+@RequestMapping("menu/pizzas")
+public class PizzaController extends BaseController {
+
+
+}
