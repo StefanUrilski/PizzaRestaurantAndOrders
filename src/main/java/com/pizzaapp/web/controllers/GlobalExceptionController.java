0a7499253f3c733afeafb219adeeeccc7463fd38
StefanUrilski <urilskistefan@gmail.com>
2019-11-27 13:07:56 +0200

    GlobalExceptionController class created.

---
+++ src/main/java/com/pizzaapp/web/controllers/GlobalExceptionController.java
@@ -0,0 +1,22 @@
+package com.pizzaapp.web.controllers;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.servlet.ModelAndView;
+
+@ControllerAdvice
+public class GlobalExceptionController extends BaseController {
+
+    @ExceptionHandler(RuntimeException.class)
+    public ModelAndView getException(RuntimeException exception) {
+        ModelAndView modelAndView = new ModelAndView();
+        modelAndView.setViewName("/error/error-template");
+        modelAndView.addObject("message", exception.getClass().isAnnotationPresent(ResponseStatus.class)
+                ? exception.getClass().getAnnotation(ResponseStatus.class).reason()
+                : "Something went wrong.");
+
+        return modelAndView;
+    }
+}
