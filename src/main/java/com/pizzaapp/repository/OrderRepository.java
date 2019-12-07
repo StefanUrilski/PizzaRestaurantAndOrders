9b830be060e0c8844b3c7a7fa6d8ed82455d3b27
StefanUrilski <urilskistefan@gmail.com>
2019-12-07 09:02:31 +0200

    Find all orders by town query created in OrderRepository.

--- src/main/java/com/pizzaapp/repository/OrderRepository.java
+++ src/main/java/com/pizzaapp/repository/OrderRepository.java
@@ -1,12 +1,23 @@
 package com.pizzaapp.repository;
 
 import com.pizzaapp.domain.entities.Order;
+import com.pizzaapp.domain.entities.Town;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface OrderRepository extends JpaRepository<Order, String> {
 
-
+    @Query( "select o " +
+            "from Order as o " +
+            "join o.address as a " +
+            "where a.town like :town " +
+            "and o.taken = :taken " +
+            "order by o.orderedOn desc")
+    List<Order> findAllByTownAndIfTaken(@Param("town") Town town, boolean taken);
 
 }
