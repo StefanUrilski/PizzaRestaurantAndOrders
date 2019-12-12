06a3a741d811df16bc2b279b5fa00b51fa8c5d21
StefanUrilski <urilskistefan@gmail.com>
2019-12-12 17:50:36 +0200

    MenuServiceTests getAllPizzasOrderedByName method created.

--- src/test/java/com/pizzaapp/service/integration/MenuServiceTests.java
+++ src/test/java/com/pizzaapp/service/integration/MenuServiceTests.java
@@ -3,6 +3,7 @@ package com.pizzaapp.service.integration;
 import com.pizzaapp.domain.entities.items.pizza.Pizza;
 import com.pizzaapp.domain.models.service.menu.DrinkServiceModel;
 import com.pizzaapp.domain.models.service.menu.PizzaAddServiceModel;
+import com.pizzaapp.domain.models.service.menu.PizzaServiceModel;
 import com.pizzaapp.repository.menu.DrinkRepository;
 import com.pizzaapp.repository.menu.PizzaRepository;
 import com.pizzaapp.service.IngredientService;
@@ -12,8 +13,11 @@ import org.junit.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.mock.mockito.MockBean;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Optional;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
@@ -64,6 +68,23 @@ public class MenuServiceTests extends TestBase {
         menuService.addDrink(drink);
         verify(drinkRepository)
                 .save(any());
+    }
+
+    @Test
+    public void getAllPizzasOrderedByName_shouldReturnAllPizzas() {
+        Pizza p1 = new Pizza();
+        Pizza p2 = new Pizza();
+
+        List<Pizza> actual = new ArrayList<>();
+        actual.add(p1);
+        actual.add(p2);
+
+        when(pizzaRepository.findAllOrderedAlphabetically())
+                .thenReturn(actual);
+
+        List<PizzaServiceModel> expected = menuService.getAllPizzasOrderedByName();
+
+        assertEquals(actual.size(), expected.size());
     }
 
 }
