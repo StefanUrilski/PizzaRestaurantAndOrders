6d7098641d02d9d7ea5d90bda124d3bde526825c
StefanUrilski <urilskistefan@gmail.com>
2019-12-13 08:29:55 +0200

    UserServiceTests loadUserByUsername_whenUserFound_shouldReturnSameUser method created.

--- src/test/java/com/pizzaapp/service/integration/UserServiceTests.java
+++ src/test/java/com/pizzaapp/service/integration/UserServiceTests.java
@@ -1,15 +1,18 @@
 package com.pizzaapp.service.integration;
 
+import com.pizzaapp.domain.entities.User;
 import com.pizzaapp.repository.UserRepository;
 import com.pizzaapp.service.UserService;
 import com.pizzaapp.testBase.TestBase;
 import org.junit.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 
 import java.util.Optional;
 
+import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.when;
 
 public class UserServiceTests extends TestBase {
@@ -26,5 +29,20 @@ public class UserServiceTests extends TestBase {
                 .thenReturn(Optional.empty());
 
         userService.loadUserByUsername("email");
+    }
+
+    @Test
+    public void loadUserByUsername_whenUserFound_shouldReturnSameUser() {
+        User user = new User();
+        user.setUsername("someEmail");
+        user.setPassword("pass");
+
+        when(userRepository.findByUsername("email"))
+                .thenReturn(Optional.of(user));
+
+        UserDetails loadedUser = userService.loadUserByUsername("email");
+
+        assertEquals(user.getUsername(), loadedUser.getUsername());
+        assertEquals(user.getPassword(), loadedUser.getPassword());
     }
 }
